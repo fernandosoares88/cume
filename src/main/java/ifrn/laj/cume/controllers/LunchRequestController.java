c401011c16768d45b534551dd0f1e59435f44ee0
Fernando Soares <fernandolinharessoares@gmail.com>
2023-06-17 11:52:44 -0300

    Implementando restrições nas solicitações de almoço

--- src/main/java/ifrn/laj/cume/controllers/LunchRequestController.java
+++ src/main/java/ifrn/laj/cume/controllers/LunchRequestController.java
@@ -26,6 +26,8 @@ import ifrn.laj.cume.repositories.StudentRepository;
 @Controller
 @RequestMapping("/lunchrequests")
 public class LunchRequestController {
+	
+	private static final int REQUEST_HOUR_LIMIT = 16;
 
 	@Autowired
 	private LunchRequestRepository lrr;
@@ -34,17 +36,27 @@ public class LunchRequestController {
 
 	@GetMapping("/form")
 	@PreAuthorize("hasRole('STUDENT')")
-	public String form() {
+	public ModelAndView form() {
 		LocalDate today = LocalDate.now();
+		LocalDate tomorow = today.plusDays(1);
+		LocalDateTime now = LocalDateTime.now();
 
 		Student principal = getPrincipal();
 
-		Optional<LunchRequest> optLunchRequest = lrr.findByStudentAndLunchRequestDate(principal, today);
-
+		Optional<LunchRequest> optLunchRequest = lrr.findByStudentAndLunchRequestDate(principal, tomorow);
 		if (optLunchRequest.isPresent()) {
-			return "redirect:/lunchrequests/" + optLunchRequest.get().getId();
+			return new ModelAndView("redirect:/lunchrequests/" + optLunchRequest.get().getId());
 		}
-		return "lunchrequests/form";
+
+		ModelAndView md = new ModelAndView("lunchrequests/form");
+		if (now.getHour() >= REQUEST_HOUR_LIMIT) {
+			md.setViewName("lunchrequests/timeisover");
+		}
+		Optional<LunchRequest> todayLunchRequest = lrr.findByStudentAndLunchRequestDate(principal, today);
+		if (todayLunchRequest.isPresent()) {
+			md.addObject("todayLunchRequest", todayLunchRequest.get());
+		}
+		return md;
 	}
 
 	@PostMapping
@@ -53,73 +65,91 @@ public class LunchRequestController {
 		System.out.println(lunchRequestDTO);
 
 		LocalDate today = LocalDate.now();
+		LocalDate tomorow = today.plusDays(1);
 		LocalDateTime now = LocalDateTime.now();
+
+		if (now.getHour() >= REQUEST_HOUR_LIMIT) {
+			return "redirect:/lunchrequests/form";
+		}
 
 		Student principal = getPrincipal();
 
-		Optional<LunchRequest> optLunchRequest = lrr.findByStudentAndLunchRequestDate(principal, today);
+		Optional<LunchRequest> optLunchRequest = lrr.findByStudentAndLunchRequestDate(principal, tomorow);
 
 		if (optLunchRequest.isPresent()) {
-			return "redirect:lunchrequests/" + optLunchRequest.get().getId();
+			return "redirect:/lunchrequests/" + optLunchRequest.get().getId();
 		}
 
 		if (!lunchRequestDTO.isConfirmation()) {
 			attributes.addFlashAttribute("msg", "Você deve selecionar o botão de confirmação!");
-			return "redirect:lunchrequests/form";
+			return "redirect:/lunchrequests/form";
 		}
 
 		LunchRequest newRequest = new LunchRequest();
 		newRequest.setDateTimeOfRequest(now);
-		newRequest.setLunchRequestDate(today);
+		newRequest.setLunchRequestDate(tomorow);
 		newRequest.setStudent(principal);
 
 		lrr.save(newRequest);
 
-		return "redirect:lunchrequests/form";
+		return "redirect:/lunchrequests/form";
 	}
 
 	@GetMapping("/{id}")
 	@PreAuthorize("hasRole('STUDENT')")
 	public ModelAndView detailLunchRequest(@PathVariable Long id) {
-		
+
 		Optional<LunchRequest> opt = lrr.findById(id);
-		if(opt.isEmpty()) {
+		if (opt.isEmpty()) {
 			return new ModelAndView("redirect:/");
 		}
-		
+
 		Student principal = getPrincipal();
 		LunchRequest lunchRequest = opt.get();
-		
-		if(!principal.getRegistration().equals(lunchRequest.getStudent().getRegistration())) {
+
+		if (!principal.getRegistration().equals(lunchRequest.getStudent().getRegistration())) {
 			return new ModelAndView("redirect:/");
 		}
-		
+
 		ModelAndView md = new ModelAndView("lunchrequests/details");
 		md.addObject("lunchRequest", lunchRequest);
+		Optional<LunchRequest> todayLunchRequest = lrr.findByStudentAndLunchRequestDate(principal, LocalDate.now());
+		if (todayLunchRequest.isPresent()) {
+			md.addObject("todayLunchRequest", todayLunchRequest.get());
+		}
+		LocalDateTime now = LocalDateTime.now();
+		if (now.getHour() < REQUEST_HOUR_LIMIT) {
+			md.addObject("canCacel", true);
+		}
 		return md;
 	}
-	
+
 	@PostMapping("/{id}/cancel")
 	@PreAuthorize("hasRole('STUDENT')")
 	public String cancelLunchRequest(@PathVariable Long id) {
+
+		LocalDateTime now = LocalDateTime.now();
+		if (now.getHour() > REQUEST_HOUR_LIMIT) {
+			return "redirect:/lunchrequests/" + id;
+		}
 		
 		Optional<LunchRequest> opt = lrr.findById(id);
-		if(opt.isEmpty()) {
+		if (opt.isEmpty()) {
 			return "redirect:/";
 		}
-		
+
 		Student principal = getPrincipal();
 		LunchRequest lunchRequest = opt.get();
-		
-		if(!principal.getRegistration().equals(lunchRequest.getStudent().getRegistration())) {
+
+		if (!principal.getRegistration().equals(lunchRequest.getStudent().getRegistration())) {
 			return "redirect:/";
 		}
-		
+
 		lrr.delete(lunchRequest);
-		
+
 		return "redirect:/lunchrequests/form";
 	}
-	
+
 	@GetMapping
 	@PreAuthorize("hasAnyRole('ADMIN', 'ASAES', 'COORD')")
 	public ModelAndView lunchRequestsForToday() {
