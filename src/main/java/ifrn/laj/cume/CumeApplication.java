22d3e7dbee9071718a0ee8902c0a008b4f0aebdf
Fernando Soares <fernandosoares@MacBook-Air-de-Fernando.local>
2024-11-08 20:34:17 -0300

    Adicionando e editando turmas pelo administrador

--- src/main/java/ifrn/laj/cume/CumeApplication.java
+++ src/main/java/ifrn/laj/cume/CumeApplication.java
@@ -2,14 +2,13 @@ package ifrn.laj.cume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CumeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CumeApplication.class, args);
-		System.out.println(new BCryptPasswordEncoder().encode("123"));
+//		System.out.println(new BCryptPasswordEncoder().encode("123"));
 	}
 
 }
--- src/main/java/ifrn/laj/cume/models/Clazz.java
+++ src/main/java/ifrn/laj/cume/models/Clazz.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Clazz {
@@ -11,6 +12,7 @@ public class Clazz {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank
 	private String name;
 
 	public Long getId() {
